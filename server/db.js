30fe44ca78852ab48fa8ceb9ccff7392deb7f169
Georgi Chinoiv <georgi.chinov@gmail.com>
2016-10-04 20:32:13 +0300

    lost password

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  user     : 'root',
+  password : '',
+  database : ''
 });
  
 connection.connect();
