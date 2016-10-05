0b9c6003cd8e8390cf3bac89a7cbf44d9e7ec6e7
Georgi Chinoiv <georgi.chinov@gmail.com>
2016-10-05 17:14:04 +0300

    server gulp added

--- server/app.js
+++ server/app.js
@@ -1,3 +1,10 @@
-/**
- * 
- */
+var express = require("express");
+var app=express();
+
+app.get('/',function(req, res) {
+	res.send('hello there123');
+})
+
+app.listen(3000,function(){
+    console.log("Express Started on Port 3000");
+});
---
+++ server/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+
+var jsFiles = ['*.js', '**/*.js'];
+
+gulp.task('serve', function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            "PORT": 3000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options);
+});
+
+gulp.task('default', ['serve']);
