100bd78a36ad02fa610978aa219968e253e296ef
Georgi Chinoiv <georgi.chinov@gmail.com>
2016-10-09 19:18:03 +0300

    deleting extra service

--- client/dist/concat.js
+++ client/dist/concat.js
@@ -91,32 +91,6 @@ mainApp.factory('userSrv', function ($http) {
  * 
  */
 
-mainApp.factory('userSvc', function($http) {
-	var baseUrl="http://localhost:3000";
-	
-	
-	return {
-		sendLoginData: function() {
-			return $http.post(baseUrl).then(function(response) {
-				return response;
-			});
-		},
-		getUserById: function(id) {
-			return users[id];
-		},
-		addNewUser: function(newUser) {
-			return $http.post(baseUrl, {newUser:newUser});
-		},
-		updateById:function(id, updatedData) {
-			users[id] = updatedData;
-		}
-	}
-});
-
-/**
- * 
- */
-
 mainApp.controller('emailController' , function($scope, $http, $httpParamSerializerJQLike, userSrv, $location){
 	
 	$scope.lostmail =  {to: ''};
