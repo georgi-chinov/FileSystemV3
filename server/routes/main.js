d6584aefa99e34ca42cee73095224c9dc40e251f
Georgi Chinoiv <georgi.chinov@gmail.com>
2016-10-14 21:19:40 +0300

    renaming the uploaded files and adding it's extention

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 var db = require('./../db.js');
 router.post('/', function(req, res) {
 
-    console.log(req.file);
+
+    // Renaming the uploaded file
+    var smth = req.file.originalname.split('.');
+    var filePath = './uploads/' + req.file.filename
+    fs.rename(filePath, filePath + '.' + smth.pop())
+
     res.send();
 });
 module.exports = router;
