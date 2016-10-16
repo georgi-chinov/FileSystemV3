e96f8e01ea8350c68c1ff8056117c156b9b5c323
Georgi Chinoiv <georgi.chinov@gmail.com>
2016-10-16 14:04:57 +0300

    sucessfully changing password server side

--- client/dist/concat.js
+++ client/dist/concat.js
@@ -701,63 +701,6 @@ mainApp.controller('homeController' , function($rootScope,$scope){
 	$rootScope.showCarousel = false;
 })
 /**
- * 
- */
-mainApp.controller('loginController',function($scope, $rootScope, $location,userSrv){
-	
-	$scope.user = {name:'', password:''};
-	$rootScope.showCarousel = false;
-	//show message
-	$scope.showModal = function(){
-		if(	$scope.show == true){
-			$scope.show = false ;
-		} else {
-			$scope.show = true ;
-		}
-	}
-	//check whether the username is valid 
-	$scope.isValidName = function(){
-		var userReg = new RegExp(/^[a-zA-Z0-9.\-_$@*!]{5,20}$/);
-		if(userReg.test($scope.user.name)){
-			return true;
-		} else {
-			return false;
-		}
-	}
-	//check whether the password is valid 
-	$scope.isValidPassword = function(){
-		var passwordReg = new RegExp(/^[a-zA-Z0-9.\-_$@*!]{8,20}$/);
-		if(passwordReg.test($scope.user.password)){
-			return true;
-		} else {
-			return false;
-		}
-	 }
-	//Checks if the data is valid
-		$scope.isValid = function(){
-			if($scope.isValidName() && $scope.isValidPassword()){
-				return false;
-			} else {
-					return true;
-			}
-		}
-	  //login function
-		 $scope.login = function(){
-			 if (!$scope.isValid()) {
-					userSrv.userLogin($scope.user).then(function(response){
-						if(response.data == "Logged!"){
-				        	console.log("probe when logged")
-							$location.path('/main');
-					
-				        } else if(response.data == "No such user!") {
-				        		$scope.showModal();
-				        		console.log("probe when NOT logged");
-				        }
-				   })
-			 }
-		 }	
-	})
-/**
  *
  */
 
@@ -1003,6 +946,63 @@ function buildTree() {
 /**
  * 
  */
+mainApp.controller('loginController',function($scope, $rootScope, $location,userSrv){
+	
+	$scope.user = {name:'', password:''};
+	$rootScope.showCarousel = false;
+	//show message
+	$scope.showModal = function(){
+		if(	$scope.show == true){
+			$scope.show = false ;
+		} else {
+			$scope.show = true ;
+		}
+	}
+	//check whether the username is valid 
+	$scope.isValidName = function(){
+		var userReg = new RegExp(/^[a-zA-Z0-9.\-_$@*!]{5,20}$/);
+		if(userReg.test($scope.user.name)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	//check whether the password is valid 
+	$scope.isValidPassword = function(){
+		var passwordReg = new RegExp(/^[a-zA-Z0-9.\-_$@*!]{8,20}$/);
+		if(passwordReg.test($scope.user.password)){
+			return true;
+		} else {
+			return false;
+		}
+	 }
+	//Checks if the data is valid
+		$scope.isValid = function(){
+			if($scope.isValidName() && $scope.isValidPassword()){
+				return false;
+			} else {
+					return true;
+			}
+		}
+	  //login function
+		 $scope.login = function(){
+			 if (!$scope.isValid()) {
+					userSrv.userLogin($scope.user).then(function(response){
+						if(response.data == "Logged!"){
+				        	console.log("probe when logged")
+							$location.path('/main');
+					
+				        } else if(response.data == "No such user!") {
+				        		$scope.showModal();
+				        		console.log("probe when NOT logged");
+				        }
+				   })
+			 }
+		 }	
+	})
+/**
+ * 
+ */
 mainApp.controller ('registerController',function($rootScope,$scope, userSrv  , $location){
 	
 	$scope.user = {name:'',password:'' , passRepeated:'' , email:''};
@@ -1128,20 +1128,20 @@ mainApp.controller('editController', function($rootScope, $scope, userSrv) {
         }
         //send new password and before that check whether  the old is the current user's password in the db
     $scope.sendNewPass = function() {
+        console.log($scope.info);
         if (!$scope.isValidInfo()) {
-            userSrv.userCheckpw({
-                oldpass: $scope.info.oldPassword
-            }).then(function(response) {
-                if (response.data && response.data.legit == true) {
-                    userSrv.userChangepw($scope.info.password).then(function(response) {
-
-                    })
-                }
-            });
+            userSrv.userCheckpw($scope.info)
         }
     }
 })
 
+/**
+ * 
+ */
+
+mainApp.controller('sharedController', function($scope,$rootScope) {
+	
+})
 /**
  * 
  */
